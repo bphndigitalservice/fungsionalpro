93294964c0eda4b33a351eaeb24a9750460982ee
fransfilastap@live.com <fransfilastap@Live.com>
2025-02-05 12:32:11 +0700

    Refactor verification workspaces and simplify migrations.
    Made `id` columns visible by default in tables across verification workspaces. Adjusted verifier access queries with limits and improved join consistency. Removed unnecessary `submission_bag_id` field from migration.

--- app/Filament/Pages/Verification/PointSubmissionVerificationWorkspace.php
+++ app/Filament/Pages/Verification/PointSubmissionVerificationWorkspace.php
@@ -36,7 +36,7 @@ class PointSubmissionVerificationWorkspace extends Page implements HasInfolists,
     {
         return $table
             ->columns([
-                TextColumn::make('id')->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('id')->toggleable(isToggledHiddenByDefault: false),
                 TextColumn::make('client.identity.name')->label('Nama'),
                 TextColumn::make('client.nip')->label('NIP'),
                 TextColumn::make('client.agenciable.name')->label('Instansi'),
@@ -46,13 +46,13 @@ class PointSubmissionVerificationWorkspace extends Page implements HasInfolists,
                 TextColumn::make('verified_at')->toggleable(isToggledHiddenByDefault: true),
             ])
             ->actions([
-                VerifyPointSubmissionAction::make()->hidden(fn (Model $record) => ! static::canVerifying() || $record->status == PointSubmissionStatus::Verified),
+                VerifyPointSubmissionAction::make()->hidden(fn(Model $record) => !static::canVerifying() || $record->status == PointSubmissionStatus::Verified),
             ]);
     }
 
     public static function canVerifying(): bool
     {
-        return ! auth()->user()->isSuperAdmin() || ! auth()->user()->hasRole('verifier');
+        return !auth()->user()->isSuperAdmin() || !auth()->user()->hasRole('verifier');
     }
 
     public static function canView(): bool
@@ -62,7 +62,7 @@ class PointSubmissionVerificationWorkspace extends Page implements HasInfolists,
 
     protected function getTableQuery(): Builder|Relation|null
     {
-        $verifierAccess = VerifierAccess::query()->where('user_id', auth()->user()->id);
+        $verifierAccess = VerifierAccess::query()->where('user_id', auth()->user()->id)->limit(1);
 
         return ClientPointSubmission::leftJoin('clients', 'client_id', '=', 'client_point_submissions.client_id')
             ->joinSub($verifierAccess, 'va', function (JoinClause $join) {
@@ -87,14 +87,15 @@ class PointSubmissionVerificationWorkspace extends Page implements HasInfolists,
         return __('labels.page.v_client_point_submission.title');
     }
 
+
     public function getTabs(): array
     {
         return [
             'all' => Tab::make(__('All')),
             'new' => Tab::make(__('New'))
                 ->badge($this->getTableQuery()->whereNull('client_point_submissions.verified_at')->count())
-                ->modifyQueryUsing(fn (Builder $query) => $query->whereNull('client_point_submissions.verified_at')),
-            'processed' => Tab::make(__('Processed'))->modifyQueryUsing(fn (Builder $query) => $query->whereNotNull('client_point_submissions.verified_at')),
+                ->modifyQueryUsing(fn(Builder $query) => $query->whereNull('client_point_submissions.verified_at')),
+            'processed' => Tab::make(__('Processed'))->modifyQueryUsing(fn(Builder $query) => $query->whereNotNull('client_point_submissions.verified_at')),
         ];
     }
 
