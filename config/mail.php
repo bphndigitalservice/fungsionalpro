81f118566e01c9e7d5cd93f5813284d55eb17bd9
krrreeesss <krresti8@gmail.com>
2026-08-05 08:31:55 +0700

    Fix: Fixing email verification configuration

--- config/mail.php
+++ config/mail.php
@@ -65,6 +65,10 @@ return [
             'path' => env('MAIL_SENDMAIL_PATH', '/usr/sbin/sendmail -bs -i'),
         ],
 
+        'resend' => [
+            'transport' => 'resend',
+        ],
+
         'log' => [
             'transport' => 'log',
             'channel' => env('MAIL_LOG_CHANNEL'),
